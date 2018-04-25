df6f111f5a337440348bdef487c575532f0cf689
Taulant Spahiu <spahiut@gmail.com>
2018-04-25 23:32:42 +0200

    wip: structure changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,22 @@ var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 
 gulp.task('browserify', function(){
-    browserify('src/js/main.js')
-        .transform('reactify')
-        .bundle()
-        .pipe(source('main.js'))
-        .pipe(gulp.dest('dist/js'))
-});
+	browserify('src/js/main.js')
+		.transform('reactify')
+		.bundle()
+		.pipe(source('main.js'))
+		.pipe(gulp.dest('dist/js'))
+})
 
 gulp.task('copy', function(){
-    gulp.src('src/index.html')
-        .pipe(gulp.dest('dist'));
-    gulp.src('src/css/*')
-        .pipe(gulp.dest('dist/css'));
-    gulp.src('src/js/*')
-        .pipe(gulp.dest('dist/js'));
+	gulp.src('src/index.html')
+		.pipe(gulp.dest('dist'))
+	gulp.src('src/css/*.*')
+		.pipe(gulp.dest('dist/css'))
+	gulp.src('src/js/vendors/*.*')
+		.pipe(gulp.dest('dist/js'))
 })
 
 gulp.task('default', ['browserify', 'copy'], function(){
-    return gulp.watch('src/**/*.*', ['browserify', 'copy']);
-});
+	return gulp.watch('src/**/*.*', ['browserify', 'copy'])
+})
